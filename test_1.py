ef4a27e0d6a76b4df3f22cbae360f85ab25b8108
alexhvorostov <hvorostov.alex1@gmail.com>
2023-06-08 20:29:47 +0400

    Added next step tests

--- test_1.py
+++ test_1.py
@@ -1,5 +1,6 @@
 from testpage import OperationsHelper
 import logging
+import time
 
 
 def test_step1(browser, error_text):
@@ -8,44 +9,35 @@ def test_step1(browser, error_text):
     testpage.go_to_site()
     testpage.enter_login("test")
     testpage.enter_pass("test")
-    testpage.click_login_btn()
+    testpage.click_login_button()
     assert testpage.get_error_text() == error_text
 
-# def test_step2(x_selector1, x_selector2, btn_selector, login_verification, user_name):
-#     input_login = site.find_element("xpath", x_selector1)
-#     input_login.clear()
-#     input_login.send_keys(test_data['login'])
-#
-#     input_psw = site.find_element("xpath", x_selector2)
-#     input_psw.clear()
-#     input_psw.send_keys(test_data['password'])
-#
-#     submit_button = site.find_element("css", btn_selector)
-#     submit_button.click()
-#
-#     enter_text = site.find_element("xpath", login_verification)
-#     assert enter_text.text == user_name
-#
-#
-# def test_step3(btn_create_post, x_selector4, x_selector5, x_selector6, save_btn_post, x_selector7, check_new_post):
-#     button_create = site.find_element("xpath", btn_create_post)
-#     button_create.click()
-#
-#     input_title = site.find_element("xpath", x_selector4)
-#     input_title.send_keys(test_data["title_post"])
-#
-#     input_des = site.find_element("xpath", x_selector5)
-#     input_des.send_keys(test_data["descr_post"])
-#
-#     input_content = site.find_element("xpath", x_selector6)
-#     input_content.send_keys(test_data["content_post"])
-#
-#     time.sleep(test_data["sleep_time"])
-#     btn_save = site.find_element("xpath", save_btn_post)
-#     btn_save.click()
-#     time.sleep(test_data["sleep_time"])
-#
-#     check_title = site.find_element("xpath", x_selector7)
-#     assert check_title.text == check_new_post
-#
-#     site.close_connect()
+
+def test_step2(browser, check_enter):
+    logging.info("Test2 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.go_to_site()
+    testpage.enter_login("ivanivanov697")
+    testpage.enter_pass("bb5e16295e")
+    testpage.click_login_button()
+    assert testpage.check_enter_user() == check_enter
+
+
+def test_step3(browser, form_title):
+    logging.info("Test3 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.button_contact()
+    time.sleep(3)
+    assert testpage.check_open_form() == form_title
+
+
+def test_step4(browser, alert_text):
+    logging.info("Test4 Starting")
+    testpage = OperationsHelper(browser)
+    testpage.button_contact()
+    testpage.input_form_name("Your name")
+    testpage.input_form_email("test@email.ru")
+    testpage.input_form_content("Enter your content")
+    testpage.contact_us_btn()
+    time.sleep(3)
+    assert testpage.check_alert_text() == alert_text
